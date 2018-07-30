4e1ff042e758f5ead7d7c35fcef6acb5b46c24f1
tstillwell2k17 <tristanstillwell@gmail.com>
2018-07-29 23:10:40 -0400

    fix: Make class and function definitions use valid syntax

--- neos.py
+++ neos.py
@@ -7,7 +7,7 @@ NASA_API_KEY = 'DEMO_KEY'  # replace DEOM_KEY with your api key
 
 class NEO(object):
     """ Near Earth Object """
-    def __init__(self, name, designation, size_min, size_max)
+    def __init__(self, name, designation, size_min, size_max):
         self.name = name
         self.designation = designation
         self.distance = distance
@@ -15,9 +15,9 @@ class NEO(object):
         self.size_max = size_max
 
 
-class NEOWebService(object)
+class NEOWebService(object):
     """ Webservice API calls see https://api.nasa.gov/api.html#NeoWS """
-    def feed(self, start_date, end_date)
+    def feed(self, start_date, end_date):
         """ Retrieve a list of Asteroids
             based on their closest approach date to Earth """
         url_params = {'start_date': start_date,
@@ -26,13 +26,13 @@ class NEOWebService(object)
                      }
         feed_query = requests.get('https://api.nasa.gov/neo/rest/v1/feed', params=url_params)
         pass
-    def lookup(self, asteroid_id)
+    def lookup(self, asteroid_id):
         """ Retrieve a specific Asteroid based on its
             NASA JPL small body (SPK-ID) ID """
         url_params = {'api_key': NASA_API_KEY}
         lookup_query = results.get('https://api.nasa.gov/neo/rest/v1/neo/%s' % asteroid_id, params=url_params)
         pass
-    def browse(self)
+    def browse(self):
         """ Browse the overal Asteroid data-set """
         url_params = {'api_key': NASA_API_KEY}
         browse_query = requests.get('https://api.nasa.gov/neo/rest/v1/browse', params=url_params)
