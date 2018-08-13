bce6ffbc45254b5379d4c9599620dbff89f0fdbd
tstillwell2k17 <tristanstillwell@gmail.com>
2018-08-12 23:26:16 -0400

    refactor: Replace browse loop with processNEOs call

--- neos.py
+++ neos.py
@@ -72,10 +72,7 @@ class NEOWebService(object):
             browse_query = requests.get(endpoint_url, params=url_params)
             response_data = browse_query.json()
             near_earth_objects = response_data['near_earth_objects']
-            neo_list = []
-            for neo in near_earth_objects:  # Process each NEO from response
-                near_earth_object = populate(neo)
-                neo_list.append(near_earth_object)
+            neo_list = processNEOs(near_earth_objects)
             links = response_data['links']
             return [neo_list, links]
         except requests.exceptions.RequestException as e:
