092beb9e09b00327d87f00eede80206a97a617b2
tstillwell2k17 <tristanstillwell@gmail.com>
2018-09-19 23:51:23 -0500

    docs: Specify 'API' for 'NEOWebService' in NEO docstring

--- neows-wrapper.py
+++ neows-wrapper.py
@@ -13,7 +13,7 @@ NASA_API_KEY = 'DEMO_KEY'
 
 
 class NEO(object):
-    """ Near Earth Object retrieved from NEOWebService """
+    """ Near Earth Object retrieved from NEOWebService API """
     def __init__(self, neo_reference_id, name, **kwargs):
         self.neo_reference_id = neo_reference_id
         self.name = name
