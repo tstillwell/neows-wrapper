9d37ed09baadf98fbc9aeae73b9fc698bca549c0
tstillwell2k17 <tristanstillwell@gmail.com>
2018-09-19 23:42:09 -0500

    docs: Make processNEOs docstring more clear

--- neows-wrapper.py
+++ neows-wrapper.py
@@ -44,7 +44,7 @@ class NEOWebService(object):
                    )
 
     def processNEOs(self, json_neo_list):
-        """ Make a list of NEOs by calling populate on json response """
+        """ Make a list of NEOs by calling populate on json near_earth_objects """
         neo_list = []
         for neo in json_neo_list:  # Process each NEO from response
             near_earth_object = self.populate(neo)
